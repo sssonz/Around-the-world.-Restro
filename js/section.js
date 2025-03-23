fd77d8efdf30ea46e5b69f58b1925562d9ca1ed3
Sonya <svanpilogova@edu.hse.ru>
2025-03-23 21:51:30 +0300

    editing a screen

--- js/section.js
+++ js/section.js
@@ -4,9 +4,36 @@ document.addEventListener("DOMContentLoaded", function() {
     const sections = document.querySelectorAll('div[class^="section"]');
     let isFirstSectionVisible = true; // Флаг для отслеживания состояния первой секции
 
+    function updateSections() {
+        if (window.matchMedia("(max-width: 415px) and (orientation: portrait)").matches) {
+            // Если портретная ориентация и ширина <= 415px
+            sections.forEach((section, i) => {
+                if (i === 0) {
+                    section.classList.remove('sectionnone'); // Показать первую секцию
+                } else {
+                    section.classList.add('sectionnone'); // Скрыть остальные секции
+                }
+            });
+        } 
+        else {
+            // Если альбомная ориентация или ширина > 415px
+            sections.forEach((section, i) => {
+                if (i === 1) {
+                    section.classList.remove('sectionnone'); // Показать вторую секцию
+                } else {
+                    section.classList.add('sectionnone'); // Скрыть остальные секции
+                }
+            });
+        }
+    }
+
+    window.addEventListener('resize', updateSections);
+
+    updateSections();
+    
     function showAllSections() {
         sections.forEach((section, i) => {
-            if (i === 0) {
+            if (i === 1) {
                 section.classList.add('sectionnone'); // Скрыть первую секцию
             } else {
                 section.classList.remove('sectionnone'); // Показать остальные секции
@@ -27,13 +54,5 @@ document.addEventListener("DOMContentLoaded", function() {
             isFirstSectionVisible = false; // Обновляем флаг
         }
     });
-
-    // Инициализация: показываем только первую секцию при загрузке
-    sections.forEach((section, i) => {
-        if (i === 0) {
-            section.classList.remove('sectionnone'); // Показать первую секцию
-        } else {
-            section.classList.add('sectionnone'); // Скрыть остальные секции
-        }
-    });
 });
+    
